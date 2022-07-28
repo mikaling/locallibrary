bb31fae9874315aacbadf3421509810f59b39c73
Akim Kingori <akim.king12@gmail.com>
2022-07-28 15:07:45 +0300

    Register ModelAdmin classes with models

--- catalog/admin.py
+++ catalog/admin.py
@@ -3,8 +3,25 @@ from .models import  Author, Genre, Book, BookInstance, Language
 
 # Register your models here.
 
-admin.site.register(Book)
-admin.site.register(Author)
+# admin.site.register(Book)
+# admin.site.register(Author)
 admin.site.register(Genre)
-admin.site.register(BookInstance)
+# admin.site.register(BookInstance)
 admin.site.register(Language)
+
+# define the admin class
+class AuthorAdmin(admin.ModelAdmin):
+    pass
+
+# register the admin class with the associated model
+admin.site.register(Author, AuthorAdmin)
+
+# register admin class for Book usint the decorator
+@admin.register(Book)
+class BookAdmin(admin.ModelAdmin):
+    pass
+
+@admin.register(BookInstance)
+class BookInstanceAdmin(admin.ModelAdmin):
+    pass
+
